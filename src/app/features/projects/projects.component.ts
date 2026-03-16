641f3da212437d52675e85228ccf759cfa2d1f04
Kunal <shivamgeu7@gmail.com>
2026-03-16 19:30:23 +0530

    added links to client section

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Client {
+  name: string;
+  description: string;
+  iconBgClass: string;
+  iconPath: string;
+  link: string;
+  image?: string;
+}
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  clients: Client[] = [
+    {
+      name: 'Flowerdale school',
+      description: 'Educational excellence and digital transformation for modern learning environments.',
+      iconBgClass: 'from-blue-400 to-indigo-600',
+      iconPath: 'M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253',
+      link: 'https://flowerdaleschool.com/'
+    },
+    {
+      name: 'Swirl wines',
+      description: 'Premium wine distribution and inventory management solutions.',
+      iconBgClass: 'from-purple-500 to-pink-600',
+      iconPath: 'M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z',
+      link: 'https://play.google.com/store/apps/details?id=com.swirl.yourwines&hl=en'
+    },
+    {
+      name: 'current diary ERP',
+      description: 'Smart dairy management and enterprise resource planning system.',
+      iconBgClass: 'from-green-400 to-blue-500',
+      iconPath: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01',
+      link: 'https://play.google.com/store/apps/details?id=info.currentDiary.com&hl=en_IN'
+    }
+  ];
 }
